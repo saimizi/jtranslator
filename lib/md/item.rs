4ece19324e7e3da368b01034a759d847c1ceb639
Seimizu Joukan <joukan.seimizu@gmail.com>
2024-11-05 22:32:21 +0900

    Imporve item module.

--- lib/md/item.rs
+++ lib/md/item.rs
@@ -1,3 +1,5 @@
+use std::usize;
+
 use super::MdOperation;
 #[allow(unused)]
 use super::{MarkdownParser, Rule};
@@ -5,10 +7,11 @@ use super::{MarkdownParser, Rule};
 use jlogger_tracing::jdebug;
 #[allow(unused)]
 use pest::Parser;
+use pest::Token;
 
 #[allow(unused)]
 pub struct Item {
-    element: Vec<Box<dyn MdOperation>>,
+    text: String,
     mark: Option<char>,
     level: usize,
 }
@@ -16,16 +19,51 @@ pub struct Item {
 impl Item {
     pub fn parse(input: &str) -> Option<(Self, &str)> {
         if let Ok(pair) = MarkdownParser::parse(Rule::list, input) {
-            let left = &input[pair.as_str().len()..];
+            let mut item_end = 0_usize;
+            let mut mark_start = 0_usize;
+            let mut mark_end = 0_usize;
+            let mut is_number_item = false;
 
-            Some((
-                Self {
-                    element: Vec::new(),
-                    mark: None,
-                    level: 0,
-                },
-                left,
-            ))
+            for token in pair.tokens() {
+                match token {
+                    Token::Start { rule, pos } => {
+                        if rule == Rule::non_number_item_mark {
+                            mark_start = pos.pos();
+                        }
+
+                        if rule == Rule::number_item_mark {
+                            mark_start = pos.pos();
+                        }
+                    }
+                    Token::End { rule, pos } => match rule {
+                        Rule::number_item => {
+                            item_end = pos.pos();
+                            is_number_item = true;
+                        }
+                        Rule::non_number_item => {
+                            item_end = pos.pos();
+                        }
+                        Rule::non_number_item_mark => {
+                            mark_end = pos.pos();
+                        }
+                        Rule::number_item_mark => {
+                            mark_end = pos.pos();
+                        }
+                        _ => {}
+                    },
+                }
+            }
+
+            let left = &input[item_end..];
+            let level = input[..mark_start].len();
+            let mark = if !is_number_item {
+                Some(input.chars().collect::<Vec<char>>()[mark_start])
+            } else {
+                None
+            };
+            let text = input[mark_end..].trim().to_owned();
+
+            Some((Self { text, mark, level }, left))
         } else {
             None
         }
